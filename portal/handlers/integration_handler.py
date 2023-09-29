e82524d797c808e8243ecbf793e4035910258c53
saijaligama <saijaligama@icloud.com>
2023-09-28 19:32:36 -0500

    addded trigonometry

--- portal/handlers/integration_handler.py
+++ portal/handlers/integration_handler.py
@@ -4,10 +4,14 @@ import sympy as sp
 def calculate_integration(data):
     expr_str = data[0]
     var_str = data[1]
-    lower_limit = data[2]
-    upper_limit = data[3]
 
     var = sp.symbols(var_str)
     expr = sp.sympify(expr_str)
-    result = sp.integrate(expr, (var, lower_limit, upper_limit))
-    return str(result)
+
+    if len(data) == 2:
+        return str(sp.integrate(expr,var))
+    else:
+        lower_limit = data[2]
+        upper_limit = data[3]
+        result = sp.integrate(expr, (var, lower_limit, upper_limit))
+        return str(result)
